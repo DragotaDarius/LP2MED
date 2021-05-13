61ff97803ac0eb14b2e93bc2ae2f3c059b16933c
DryZus <41472264+DryZus@users.noreply.github.com>
2021-05-13 11:03:42 +0300

    Update encode-decode.py
    Added Algorithm  list selection

--- encode-decode.py
+++ encode-decode.py
@@ -3,105 +3,109 @@
 from tkinter import *
 import base64
 
-#initialize window
+# initialize window
 root = Tk()
 root.geometry('500x300')
-root.resizable(0,0)
+root.resizable(0, 0)
 
-#title of the window
+# title of the window
 root.title("DataFlair - Message Encode and Decode")
 
+# label
 
+Label(root, text='ENCODE DECODE', font='arial 20 bold').pack()
+Label(root, text='DataFlair', font='arial 20 bold').pack(side=BOTTOM)
 
-#label
-
-Label(root, text ='ENCODE DECODE', font = 'arial 20 bold').pack()
-Label(root, text ='DataFlair', font = 'arial 20 bold').pack(side =BOTTOM)
-
-
-#define variables
+# define variables
 
 Text = StringVar()
 private_key = StringVar()
 mode = StringVar()
 Result = StringVar()
+RadioButton = StringVar()
 
 
 #######define function#####
 
-#function to encode
+# function to encode
 
-def Encode(key,message):
-    enc=[]
+def Encode(key, message):
+    enc = []
     for i in range(len(message)):
         key_c = key[i % len(key)]
         enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
-        
+
     return base64.urlsafe_b64encode("".join(enc).encode()).decode()
 
-#function to decode
 
-def Decode(key,message):
-    dec=[]
+# function to decode
+
+def Decode(key, message):
+    dec = []
     message = base64.urlsafe_b64decode(message).decode()
     for i in range(len(message)):
         key_c = key[i % len(key)]
-        dec.append(chr((256 + ord(message[i])- ord(key_c)) % 256))
-        
+        dec.append(chr((256 + ord(message[i]) - ord(key_c)) % 256))
+
     return "".join(dec)
 
-#function to set mode
+
+# function to set mode
 def Mode():
-    if(mode.get() == 'e'):
+    if (mode.get() == 'e'):
         Result.set(Encode(private_key.get(), Text.get()))
-    elif(mode.get() == 'd'):
+    elif (mode.get() == 'd'):
         Result.set(Decode(private_key.get(), Text.get()))
     else:
         Result.set('Invalid Mode')
 
 
+# Function to exit window
 
-#Function to exit window
-        
 def Exit():
     root.destroy()
 
 
-#Function to reset
+# Function to reset
 def Reset():
     Text.set("")
     private_key.set("")
     mode.set("")
     Result.set("")
 
+def Change():
+    print("TEST")
 
 #################### Label and Button #############
 
-#Message
-Label(root, font= 'arial 12 bold', text='MESSAGE').place(x= 60,y=60)
-Entry(root, font = 'arial 10', textvariable = Text, bg = 'ghost white').place(x=290, y = 60)
+# Message
+Label(root, font='arial 12 bold', text='MESSAGE').place(x=60, y=60)
+Entry(root, font='arial 10', textvariable=Text, bg='ghost white').place(x=290, y=60)
 
-#key
-Label(root, font = 'arial 12 bold', text ='KEY').place(x=60, y = 90)
-Entry(root, font = 'arial 10', textvariable = private_key , bg ='ghost white').place(x=290, y = 90)
+# key
+Label(root, font='arial 12 bold', text='KEY').place(x=60, y=90)
+Entry(root, font='arial 10', textvariable=private_key, bg='ghost white').place(x=290, y=90)
 
+# mode
+Label(root, font='arial 12 bold', text='MODE(e-encode, d-decode)').place(x=60, y=120)
+Entry(root, font='arial 10', textvariable=mode, bg='ghost white').place(x=290, y=120)
 
-#mode
-Label(root, font = 'arial 12 bold', text ='MODE(e-encode, d-decode)').place(x=60, y = 120)
-Entry(root, font = 'arial 10', textvariable = mode , bg= 'ghost white').place(x=290, y = 120)
-
-
-
-#result
-Entry(root, font = 'arial 10 bold', textvariable = Result, bg ='ghost white').place(x=290, y = 150)
+# result
+Entry(root, font='arial 10 bold', textvariable=Result, bg='ghost white').place(x=290, y=150)
 
 ######result button
-Button(root, font = 'arial 10 bold', text = 'RESULT'  ,padx =2,bg ='LightGray' ,command = Mode).place(x=60, y = 150)
+Button(root, font='arial 10 bold', text='RESULT', padx=2, bg='LightGray', command=Mode).place(x=60, y=150)
 
+# reset button
+Button(root, font='arial 10 bold', text='RESET', width=6, command=Reset, bg='LimeGreen', padx=2).place(x=80, y=190)
 
-#reset button
-Button(root, font = 'arial 10 bold' ,text ='RESET' ,width =6, command = Reset,bg = 'LimeGreen', padx=2).place(x=80, y = 190)
+# change encription button
+Button(root, font='arial 10 bold', text='CHANGE', width=6, command=Change, bg='LimeGreen', padx=2).place(x=280, y=190)
 
-#exit button
-Button(root, font = 'arial 10 bold',text= 'EXIT' , width = 6, command = Exit,bg = 'OrangeRed', padx=2, pady=2).place(x=180, y = 190)
+# exit button
+Button(root, font='arial 10 bold', text='EXIT', width=6, command=Exit, bg='OrangeRed', padx=2, pady=2).place(x=180, y=190)
+
+Radiobutton(root, font='arial 10 bold', text='First',command=Change, value='something').place(x=380, y =190)
+Radiobutton(root, font='arial 10 bold', text='Second',command=Change, value='bla').place(x=380, y =190+20)
+
 root.mainloop()
